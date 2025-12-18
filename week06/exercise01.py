58cf1daa6d426b4ac3129f0b22fa5741c6015835
George Kantasis <kantasisg@gmail.com>
2025-12-18 19:53:08 +0200

    week06 WIP

---
+++ week06/exercise01.py
@@ -0,0 +1,35 @@
+import json
+import numpy as np
+
+filename_str = 'forecast.json'
+with open(filename_str,'r') as f:
+   data_str = f.read()
+
+
+data_dict = json.loads(data_str)
+
+time_list = data_dict['hourly']['time']
+temp_list = data_dict['hourly']['temperature_2m']
+
+matrix = []
+
+matrix_np = np.array([time_list, temp_list])
+
+matrix_np = matrix_np.T
+
+print(matrix_np)
+
+# np.savetxt('forecast.csv', matrix_np,fmt='%s')
+
+
+
+
+
+
+
+
+
+
+
+
+
