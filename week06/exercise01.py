e29cc6ea8ecf3be8f23f81a6e19a44b102bf2ff6
C Enthusiast <44611490+CEnthusiast@users.noreply.github.com>
2025-12-18 20:14:34 +0200

    Add files via upload

--- week06/exercise01.py
+++ week06/exercise01.py
@@ -1,35 +1,30 @@
-import json
+import requests
 import numpy as np
 
-filename_str = 'forecast.json'
-with open(filename_str,'r') as f:
-   data_str = f.read()
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+"latitude": 40.6436,
+"longitude": 22.9309,
+"hourly": "temperature_2m",
+"start_date": "2025-12-19",
+"end_date": "2025-12-19"
+}
 
+response = requests.get(url, params=params)
+print(response.status_code)
+#print(response.text)
 
+data_str = response.text
+print(data_str)
 data_dict = json.loads(data_str)
+#print(data_dict)
+#type(data_dict)
 
-time_list = data_dict['hourly']['time']
-temp_list = data_dict['hourly']['temperature_2m']
-
-matrix = []
-
-matrix_np = np.array([time_list, temp_list])
-
-matrix_np = matrix_np.T
-
-print(matrix_np)
-
-# np.savetxt('forecast.csv', matrix_np,fmt='%s')
+matrix_np = []
 
 
+for i in range(len(data_dict['hourly']['temperature_2m'])):
+  matrix_np.append((data_dict['hourly']['time'][i],data_dict['hourly']['temperature_2m'][i]))
 
-
-
-
-
-
-
-
-
-
-
+a = np.asarray(matrix_np)
+np.savetxt("tomorrowforecast.csv", a, delimiter=",",fmt="%s")
